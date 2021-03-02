4649249f7e004c1b6f76928720d70f8887e80cf4
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-03-02 14:00:57 -0700

    Add test for current_high_bid in Item class; passing.

--- test/item_test.rb
+++ test/item_test.rb
@@ -28,4 +28,12 @@ class ItemTest < Minitest::Test
                 }
     assert_equal expected, @item1.bids
   end
+
+  def test_it_knows_current_highest_bid
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    @item1.add_bid(attendee2, 20)
+    @item1.add_bid(attendee1, 22)
+    assert_equal 22, @item1.current_high_bid
+  end
 end
